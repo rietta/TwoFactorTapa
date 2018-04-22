f6ad5e73b3dd0ed932ca01e0e012a61004d95d57
Frank Rietta <frank@rietta.com>
2018-04-22 07:40:30 -0400

    Changing theme from secret lair to private tapas. Adding Ruby versions for both ASDF and rbenv.

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resource :secret_lair, only: [:show]
+  resource :private_tapas, only: [:show]
 end
